c07c6b11f22efc4ccb9eeaf3fd2691c48a7d86ef
christianfelps <christianfelipialvim@gmail.com>
2025-07-09 16:50:01 -0300

    controller, database, shared

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server/server");
-server_1.server.listen(process.env.PORT || 3333, () => console.log(`server na url http://localhost:${process.env.PORT || 3333}`));
--- dist/server/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const http_status_codes_1 = require("http-status-codes");
-const router = (0, express_1.Router)();
-router.get('/', (req, res) => {
-    return res.status(http_status_codes_1.StatusCodes.CONFLICT).send("Ola mundo");
-});
-exports.default = router;
